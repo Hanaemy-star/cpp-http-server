3cc99b834b3245574dab63d3bd81194a536d09ec
Hanaemy <av-danilkinin@mail.ru>
2025-08-18 23:11:58 +0300

    feat: add basic TCP server~

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,25 @@
-//
-// Created by User on 16.08.2025.
-//
+#include <iostream>
+#include <boost/asio.hpp>
+
+using namespace boost::asio;
+using ip::tcp;
+
+int main() {
+    try {
+        io_context io;
+
+        tcp::acceptor acceptor(io, tcp::endpoint(tcp::v4(), 8080));
+        std::cout << "Server started at http://localhost:8080\n";
+
+        tcp::socket socket(io);
+        acceptor.accept(socket);
+        std::cout << "Client connected!\n";
+    } catch (std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+    return 0;
+}
+
+
+
+
